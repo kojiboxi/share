2f796031295b1f020a3952874b468351152b67a6
kojiboxee <yamahiro01191990@gmail.com>
2021-05-02 22:56:12 +0900

    third-commit

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -32,7 +32,7 @@ export default {
   methods: {
     auth() {
       axios
-        .post("herokuのURL/api/register", {
+        .post("https://radiant-cliffs-66144.herokuapp.com/api/register", {
           name: this.name,
           profile: this.profile,
           email: this.email,
